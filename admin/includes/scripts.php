2d285272917fa618bbff0223125033cf8a338184
ImV4l <116868991+ImV4l@users.noreply.github.com>
2024-11-29 22:38:59 +0800

    Update scripts.php

--- admin/includes/scripts.php
+++ admin/includes/scripts.php
@@ -1,12 +1,18 @@
-        <!-- <script src="js/jquery-3.7.1.min.js"></script>
+        <script src="js/jquery-3.7.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         <script src="js/dataTables.min.js" crossorigin="anonymous"></script>
-        <script>
-                let table = new DataTable('#myTabl');
-        </script>
-        <script src="js/datatables-simple-demo.js"></script>
+
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+  $(document).ready( function () {
+    $('#myTable').DataTable();
+  });
+</script>
+<script src="js/datatables-simple-demo.js"></script>
 
         </body>
 
-        </html> -->
+        </html>
