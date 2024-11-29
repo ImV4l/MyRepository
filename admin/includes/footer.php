36aadc007425c5b4fab079d31469ab2993e6ebf6
ImV4l <116868991+ImV4l@users.noreply.github.com>
2024-11-29 22:39:27 +0800

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,5 @@
  
- <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<!--  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
 
@@ -7,7 +7,7 @@
   $(document).ready( function () {
     $('#myTable').DataTable();
   });
-</script>
+</script> -->
 
  <!--        </main>
         <footer class="py-4 bg-light mt-auto">
